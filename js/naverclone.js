5a934a85d2ade1e264a7be69e72b869e1f17b6d7
jwk <nakazjk@naver.com>
2023-07-22 10:03:31 +0900

    js edit
    JavaScript fixes to increase memory efficiency

--- js/naverclone.js
+++ js/naverclone.js
@@ -1,27 +1,29 @@
+/* 전역변수를 선언하면 메모리에 계속 남아있기때문에 최대한 함수안에서 변수를 선언해 메모리효율을 늘리도록 하자 */
+
 /* 트라이앵글 버튼 요소 가져오기 */
-var triangle_button = document.getElementById("header_searchbox_image_triangle");
-/* 숨겨진 블록 가져오기 */
-var hidden_block = document.getElementById("search_hidden");
+const triangle_button = document.getElementById("header_searchbox_image_triangle");
 /* 동작할 함수 만들기 */
 function show_hidden_block() {
+    /* 숨겨진 블록 가져오기 */
+    const hidden_block = document.getElementById("search_hidden");
     if (hidden_block.style.display === "flex") {
         hidden_block.style.display = "none";
     } else {
         hidden_block.style.display = "flex";
     }
-
 }
 /* 트라이앵글 버튼에 클릭 이벤트 추가 */
 triangle_button.addEventListener("click", show_hidden_block);
 
-/* top_left_button 에 상수 설정 */
+
 const top_left_button = document.querySelector('.top_left_button');
-const top_left_button_menu = document.querySelector('#top_left_button_menu')
-const text_box_under_mail = document.querySelector('.text_box_under_mail');
-const text_box_under_cafe = document.querySelector('.text_box_under_cafe');
-const search_N_logo = document.querySelector('.search_N_logo');
+
 
 top_left_button.addEventListener('click', () => {
+    const top_left_button_menu = document.querySelector('#top_left_button_menu')
+    const text_box_under_mail = document.querySelector('.text_box_under_mail');
+    const text_box_under_cafe = document.querySelector('.text_box_under_cafe');
+    const search_N_logo = document.querySelector('.search_N_logo');
     top_left_button.classList.toggle('focus');
     top_left_button.focus();
 
@@ -41,6 +43,10 @@ top_left_button.addEventListener('click', () => {
 });
 
 document.addEventListener('click', (event) => {
+    const top_left_button_menu = document.querySelector('#top_left_button_menu')
+    const text_box_under_mail = document.querySelector('.text_box_under_mail');
+    const text_box_under_cafe = document.querySelector('.text_box_under_cafe');
+    const search_N_logo = document.querySelector('.search_N_logo');
     if (!top_left_button.contains(event.target) && !top_left_button_menu.contains(event.target)) {
         top_left_button_menu.classList.remove('show');
         text_box_under_mail.style.zIndex = '200';
