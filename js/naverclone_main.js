6803e6f9835b7ca6096fff662b20ff4833c7c3ac
jwk <nakazjk@naver.com>
2023-07-23 13:57:59 +0900

    search_area edit
    Added all services below on search_text_box_area.
    Change search_area from multiple divs into one.

--- js/naverclone_main.js
+++ js/naverclone_main.js
@@ -20,12 +20,12 @@ const top_left_button = document.querySelector('.top_left_button');
 
 top_left_button.addEventListener('click', () => {
     const top_left_button_menu = document.querySelector('#top_left_button_menu')
-    const text_box_under_mail = document.querySelector('.text_box_under_mail');
-    const text_box_under_cafe = document.querySelector('.text_box_under_cafe');
-    const search_N_logo = document.querySelector('.search_N_logo');
+    const text_box_under_mail = document.querySelector('#text_box_under_mail');
+    const text_box_under_cafe = document.querySelector('#text_box_under_cafe');
+    const search_N_logo = document.querySelector('#search_logo');
     top_left_button.classList.toggle('focus');
     top_left_button.focus();
-    
+
     /* 클릭하면 top_left_button_iframe 의 top_left_service_search_area.style.display = 'none'; 처리 */
     /* 클릭하면 top_left_button_menu_body.classList.remove('display') */
     let top_left_button_iframe = document.querySelector('#top_left_button_iframe');
@@ -50,16 +50,56 @@ top_left_button.addEventListener('click', () => {
     }
 });
 
-document.addEventListener('click', (event) => {
-    const top_left_button_menu = document.querySelector('#top_left_button_menu')
-    const text_box_under_mail = document.querySelector('.text_box_under_mail');
-    const text_box_under_cafe = document.querySelector('.text_box_under_cafe');
-    const search_N_logo = document.querySelector('.search_N_logo');
-    if (!top_left_button.contains(event.target) && !top_left_button_menu.contains(event.target)) {
+
+
+/* #text_box_under_etcbutton 을 클릭하면 .text_box_under_menu_area 에 text_box_under_menu_area_show class 를 추가하고
+ #search_hidden_allservice 의 display를 block으로 바꿈. 재클릭하면 class 제거하고 display none으로 바꿈*/
+const text_box_under_etcbutton = document.querySelector('#text_box_under_etcbutton');
+
+text_box_under_etcbutton.addEventListener('click', function (event) {
+    const text_box_under_menu_area = document.querySelector('.text_box_under_menu_area');
+    const search_hidden_allservice = document.querySelector('#search_hidden_allservice');
+    if (text_box_under_etcbutton.style.backgroundImage == '' || text_box_under_etcbutton.style.backgroundImage == 'url("../image/header_hotkey_allservice.png")') {
+        text_box_under_etcbutton.style.backgroundImage = 'url("../image/header_hotkey_allservice_x.png';
+        text_box_under_menu_area.classList.add('text_box_under_menu_area_show');
+        search_hidden_allservice.style.display = 'block';
+    } else {
+        text_box_under_etcbutton.style.backgroundImage = 'url("../image/header_hotkey_allservice.png")';
+        text_box_under_menu_area.classList.remove('text_box_under_menu_area_show');
+        search_hidden_allservice.style.display = 'none';
+    }
+
+});
+
+
+document.addEventListener('click', function(event) {
+    /* top_left_button과 top_left_button_menu 바깥 영역을 클릭하면 top_left_button_menu의 show class를 제거함  */
+    const top_left_button_menu = document.querySelector('#top_left_button_menu');
+    const text_box_under_mail = document.querySelector('#text_box_under_mail');
+    const text_box_under_cafe = document.querySelector('#text_box_under_cafe');
+    const search_N_logo = document.querySelector('#search_logo');
+    if (event.target !== top_left_button && event.target !==top_left_button_menu) {
         top_left_button_menu.classList.remove('show');
         text_box_under_mail.style.zIndex = '200';
         text_box_under_cafe.style.zIndex = '200';
         search_N_logo.style.zIndex = '200';
         top_left_button_menu.style.zIndex = '0';
+    }
+
+    /* top_left_button과 top_left_button_menu 바깥 영역을 클릭하면 top_left_button_menu의 show class를 제거함  */
+    const text_box_under_etcbutton = document.querySelector('#text_box_under_etcbutton');
+    const text_box_under_menu_area = document.querySelector('.text_box_under_menu_area');
+    const search_hidden_allservice = document.querySelector('#search_hidden_allservice');
+
+    if (event.target !== text_box_under_etcbutton && event.target !== search_hidden_allservice) {
+        text_box_under_etcbutton.style.backgroundImage = 'url("../image/header_hotkey_allservice.png")';
+        text_box_under_menu_area.classList.remove('text_box_under_menu_area_show');
+        search_hidden_allservice.style.display = 'none';
+    }
+
+    /* triangle_button과 search_hidden_block 바깥 영역을 클릭하면 search_hidden_block의 display를 none  */
+    const search_hidden_block = document.querySelector('.search_hidden_block');
+    if (event.target !== triangle_button && event.target !== search_hidden_block) {
+        search_hidden_block.style.display = 'none';
     }
 });
